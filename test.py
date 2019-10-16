7cbc90c4ed6dfd3633abc39bb4111d49e941d6c6
shagrirse <shagrirse@gmail.com>
2019-10-16 10:49:42 +0800

    i fucking hate my life

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 import csv
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 
-data = pd.read_csv('salaries_1985to2018.csv')
-df = pd.DataFrame(data, columns=['player_id', 'league'])
+data = pd.read_csv('AB_NYC_2019.csv')
+df = pd.DataFrame(data)
+new_df = df.dropna()
 
-with open('aa.csv', 'w', newline="") as a:
-    df.to_csv(a, index=False)
+checknull = new_df.isnull().sum()
 
-plt.hist(df, bins=10)
+plt.boxplot(new_df['reviews_per_month'])
+plt.show()
