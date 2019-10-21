e5059843e92a7f54f77c66d4864a37f711c714ef
shagrirse <shagrirse@gmail.com>
2019-10-21 21:13:20 +0800

    shuff

--- kkk.py
+++ kkk.py
@@ -1,11 +1,9 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
+import random
 
-data = pd.read_csv('AB_NYC_2019.csv')
-df = pd.DataFrame(data)
-df['last_review'] = pd.to_datetime(df['last_review'])
-brooklyn = df[ df['neighbourhood_group'] == 'Brooklyn' ].dropna().set_index('last_review')
-brooklyn.sort_index(inplace=True)
-brooklyn['reviews_per_month'].rolling(20).mean().plot()
-plt.show()
+data = pd.read_csv('JP10 Class List.csv')
+p = data['CLASS'].tolist()
+random.shuffle(p)
+print(p)
+a = pd.DataFrame(p)
+print(a)
