c0c9ddfc70a5242cd1656da098d07504f9e203ed
shagrirse <shagrirse@gmail.com>
2019-11-08 18:26:16 +0800

    l

---
+++ kkk.py
@@ -0,0 +1,12 @@
+import random
+
+cred = 100
+oddeven = input('Odd/Even?: ')
+betamt = input('How much would you like to bet?: ')
+game = True
+
+while game:
+    random.randint(0,10)
+
+    if oddeven == 'odd' and betamt >0:
+        
