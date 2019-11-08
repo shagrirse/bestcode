2e18b9cfedca1e612d17379210fa597119e84bdb
shagrirse <shagrirse@gmail.com>
2019-11-08 23:08:24 +0800

    ssss

--- kkk.py
+++ kkk.py
@@ -1,12 +1,19 @@
 import random
 
 cred = 100
-oddeven = input('Odd/Even?: ')
-betamt = input('How much would you like to bet?: ')
+oddeven = str(input('Odd/Even?: '))
+betamt = int(input('How much would you like to bet?: '))
 game = True
 
 while game:
-    random.randint(0,10)
-
-    if oddeven == 'odd' and betamt >0:
-        
+    number = random.randint(0,10)
+    print(number)
+    if cred > 0:
+        if oddeven == 'Odd' and (number%2 > 0):
+            print('Round Lost')
+            cred = cred - betamt
+        elif oddeven == 'Even' and (number%2 == 0):
+            print('Round Won')
+            cred = cred + betamt
+    if cred <= 0:
+        break
--- sportsday/sports.py
+++ sportsday/sports.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import numpy
 import openpyxl
+import re
 
 df = pd.read_csv(r'sportsday\PFP Sports Day Registration.csv')
 #individuals
@@ -7,10 +9,8 @@ individual = df[df['Type of registration'] == 'Individual'].dropna(axis=1)
 ind_ssc = individual[individual['Desired sport'] == "Street Soccer"]
 ind_bball = individual[individual['Desired sport'] == "3 v 3 Basketball"]
 ind_cbal = individual[individual['Desired sport'] == "Captain's Ball"]
-ind_fris = individual[individual['Desired sport'] == "Ultimate Frisbee"]
 #teams
-ultimate = df[df['Sport'] == 'Ultimate Frisbee'].dropna(axis=1)
-bball = df[df['Sport'] == '3v3 Basketball'].dropna(axis=1)
+bball = df[df['Sport'] == "3 v 3 Basketball"].dropna(axis=1)
 cbal = df[df['Sport'] == "Captain's Ball"].dropna(axis=1)
 ssc = df[df['Sport'] == "Street Soccer"].dropna(axis=1)
 # To CSV
@@ -19,9 +19,19 @@ with pd.ExcelWriter(r'sportsday\output\Compiled.xlsx') as pp:
     ind_ssc.to_excel(pp, index=False, sheet_name='Individual Street Soccer')
     ind_bball.to_excel(pp, index=False, sheet_name='Individual Basketball')
     ind_cbal.to_excel(pp, index=False, sheet_name="Individual Captain's Ball")
-    ind_fris.to_excel(pp, index=False, sheet_name='Individual Ultimate Frisbee')
 
-    ultimate.to_excel(pp, index=False, sheet_name='Ultimate Frisbee Team')
     bball.to_excel(pp, index=False, sheet_name='Basketball Team')
     cbal.to_excel(pp, index=False, sheet_name='Captains Ball Team')
     ssc.to_excel(pp, index=False, sheet_name='Street Soccer Team')
+# Admin Numbers
+col = [col for col in df.columns if "Admin" in col]
+admin_no = df[col]
+df['Number'] = df[col].apply(lambda x: ','.join(x.dropna().astype(str)), axis=1).str.replace(',', '\n')
+admin_no = df['Number'].str.replace("P", "")
+
+admin_no.to_csv(r'sportsday\admin number.csv', index=False)
+# #Execute Later
+# admin_no = pd.read_csv(r'sportsday\admin number.csv')
+# admin_no = admin_no.iloc[:,0].str.replace("P", "")
+# print(type(admin_no))
+# admin_no.to_csv(r'sportsday\admin number.csv', index=False)
